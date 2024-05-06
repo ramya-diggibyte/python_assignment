9bb103f1d52cc7f5bbca6ab83664047e75d28bf7
ramya-diggibyte <ramya.r@electrolux.com>
2024-05-06 15:20:35 +0200

    remove space in driver.py

--- src/percentage_problem/driver.py
+++ src/percentage_problem/driver.py
@@ -1,4 +1,4 @@
 #from src.percentage_problem.util import *
 from python_assignment.src.percentage_problem.util import *
-result=percent_calculation({'Ramya':[67,68,69], 'Alanis':[70,98,63],'Manoj': [52,56,60]},'Ramya')
+result=percent_calculation({'Ramya':[67,68,69], 'Alanis':[70,98,63],'Manoj':[52,56,60]},'Ramya')
 print(result)
