8e80c18e507835dbe3cc21b900e85d9bf827fc19
ramya-diggibyte <ramya.r@electrolux.com>
2024-05-07 12:21:59 +0200

    add space

--- src/percentage_problem/driver.py
+++ src/percentage_problem/driver.py
@@ -1,4 +1,4 @@
 #from src.percentage_problem.util import *
 from python_assignment.src.percentage_problem.util import *
-percentage_result =percent_calculation({'Ramya': [20,25,27,30], 'Alanis': [64,62],'Janet':[5,56]},'Ramya')
+percentage_result =percent_calculation({'Ramya': [20,25,27,30], 'Alanis': [64,62],'Janet': [5,56]},'Ramya')
 print(percentage_result)
